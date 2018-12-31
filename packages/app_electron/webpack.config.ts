12a3303028e52556149b40df18f7234d6d19da0c
Andrew Olson <andrew@olsononline.org>
2018-12-31 00:12:27 -0600

    extract buildWebpackConfig

--- packages/app_electron/webpack.config.ts
+++ packages/app_electron/webpack.config.ts
@@ -1,62 +1,12 @@
-import webpack from 'webpack'
-import WebpackDevServer from 'webpack-dev-server'
-// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
-const CreateFileWebpack = require('create-file-webpack')
-const WriteFilePlugin = require('write-file-webpack-plugin')
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { buildWebpackConfig } from '../../scripts/buildWebpackConfig';
 import pkg from "./package.json";
+const CreateFileWebpack = require('create-file-webpack')
 
-interface BuildConfigOptions {
-  name: string
-  target: webpack.Configuration['target']
-  plugins?: webpack.Configuration['plugins']
-  devServer?: webpack.Configuration['devServer']
-}
-
-const buildConfig = ({name, target, plugins, devServer}: BuildConfigOptions): webpack.Configuration & WebpackDevServer.Configuration => ({
-  target,
-  devtool: 'inline-source-map',
-  entry: `./src/${name}.ts`,
-  output: {
-    publicPath: '/',
-    filename: `${name}.js`
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: '/node_modules/',
-        use: [
-          { loader: 'babel-loader' },
-          { loader: 'ts-loader' }
-        ],
-      }
-    ]
-  },
-  plugins: [
-    ...(plugins || []),
-    new WriteFilePlugin({
-      test: /^((?!(hot-update)).)*$/
-    }),
-    // new BundleAnalyzerPlugin({
-    //   analyzerMode: 'static',
-    //   reportFilename: `stats-${name}.html`,
-    //   openAnalyzer: false,
-    // }),
-  ],
-  externals: {
-    'devtron': 'commonjs devtron',
-    'electron-react-devtools': 'commonjs electron-react-devtools',
-  },
-  stats: 'minimal',
-  devServer,
-})
+const appName = 'Ag'
 
 module.exports = [
-  buildConfig({
+  buildWebpackConfig({
     name: 'main',
     target: 'electron-main',
     plugins: [
@@ -65,7 +15,7 @@ module.exports = [
         path: 'dist',
         fileName: 'package.json',
         content: JSON.stringify({
-          name: pkg.name,
+          name: appName,
           version: pkg.version,
           main: 'main.js'
         }, null, '  ')
@@ -78,13 +28,13 @@ module.exports = [
     }
   }),
 
-  buildConfig({
+  buildWebpackConfig({
     name: 'index',
     target: 'electron-renderer',
     plugins: [
       // create index.html
       new HtmlWebpackPlugin({
-        title: '[Ag]',
+        title: `${appName}`,
       }),
     ],
     devServer: {
---
+++ scripts/buildWebpackConfig.ts
@@ -0,0 +1,56 @@
+import webpack from 'webpack';
+import WebpackDevServer from 'webpack-dev-server';
+// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
+const WriteFilePlugin = require('write-file-webpack-plugin')
+
+interface ConfigOptions {
+  name: string
+  target: webpack.Configuration['target']
+  plugins?: webpack.Configuration['plugins']
+  devServer?: webpack.Configuration['devServer']
+}
+
+export const buildWebpackConfig = ({name, target, plugins, devServer}: ConfigOptions): webpack.Configuration & WebpackDevServer.Configuration => ({
+  target,
+  devtool: 'inline-source-map',
+  entry: `./src/${name}.ts`,
+  output: {
+    publicPath: '/',
+    filename: `${name}.js`
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: '/node_modules/',
+        use: [
+          { loader: 'babel-loader' },
+          { loader: 'ts-loader' }
+        ],
+      }
+    ]
+  },
+  plugins: [
+    ...(plugins || []),
+    new WriteFilePlugin({
+      test: /^((?!(hot-update)).)*$/
+    }),
+    // new BundleAnalyzerPlugin({
+    //   analyzerMode: 'static',
+    //   reportFilename: `stats-${name}.html`,
+    //   openAnalyzer: false,
+    // }),
+  ],
+  externals: {
+    'devtron': 'commonjs devtron',
+    'electron-react-devtools': 'commonjs electron-react-devtools',
+  },
+  stats: 'minimal',
+  devServer: {
+    ...(devServer || {}),
+    stats: 'minimal',
+  },
+})
