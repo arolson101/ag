59c10d68c9e6cf92a8144b5de26c132d506d4bd4
Andrew Olson <andrew@olsononline.org>
2018-12-30 22:44:30 -0600

    electron, webpack, hot reload

--- packages/app_electron/webpack.config.ts
+++ packages/app_electron/webpack.config.ts
@@ -1,19 +1,31 @@
-import { Configuration } from 'webpack'
+import webpack from 'webpack'
+import WebpackDevServer from 'webpack-dev-server'
+// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+const CreateFileWebpack = require('create-file-webpack')
+const WriteFilePlugin = require('write-file-webpack-plugin')
+import pkg from "./package.json";
 
-const config: Configuration = {
-  devtool: "inline-source-map",
-  mode: "development",
-  entry: './index.ts',
+interface BuildConfigOptions {
+  name: string
+  target: webpack.Configuration['target']
+  plugins?: webpack.Configuration['plugins']
+  devServer?: webpack.Configuration['devServer']
+}
+
+const buildConfig = ({name, target, plugins, devServer}: BuildConfigOptions): webpack.Configuration & WebpackDevServer.Configuration => ({
+  target,
+  devtool: 'inline-source-map',
+  entry: `./src/${name}.ts`,
   output: {
-    filename: "bundle.js"
+    publicPath: '/',
+    filename: `${name}.js`
   },
   resolve: {
-    // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
   },
   module: {
     rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
       {
         test: /\.tsx?$/,
         exclude: '/node_modules/',
@@ -23,7 +35,64 @@ const config: Configuration = {
         ],
       }
     ]
-  }
-}
+  },
+  plugins: [
+    ...(plugins || []),
+    new WriteFilePlugin({
+      test: /^((?!(hot-update)).)*$/
+    }),
+    // new BundleAnalyzerPlugin({
+    //   analyzerMode: 'static',
+    //   reportFilename: `stats-${name}.html`,
+    //   openAnalyzer: false,
+    // }),
+  ],
+  externals: {
+    'devtron': 'commonjs devtron',
+    'electron-react-devtools': 'commonjs electron-react-devtools',
+  },
+  stats: 'minimal',
+  devServer,
+})
 
-module.exports = config
+module.exports = [
+  buildConfig({
+    name: 'main',
+    target: 'electron-main',
+    plugins: [
+      // create package.json
+      new CreateFileWebpack({
+        path: 'dist',
+        fileName: 'package.json',
+        content: JSON.stringify({
+          name: pkg.name,
+          version: pkg.version,
+          main: 'main.js'
+        }, null, '  ')
+      })
+    ],
+    devServer: {
+      // prevent HMR stuff getting added
+      inline: false,
+      stats: 'minimal',
+    }
+  }),
+
+  buildConfig({
+    name: 'index',
+    target: 'electron-renderer',
+    plugins: [
+      // create index.html
+      new HtmlWebpackPlugin({
+        title: '[Ag]',
+      }),
+    ],
+    devServer: {
+      publicPath: '/',
+      historyApiFallback: {
+        disableDotRule: true
+      },
+      stats: 'minimal',
+    }
+  }),
+]
