092a6e773a1b26d9823b8209eab3995b0b820fe5
Andrew Olson <andrew@olsononline.org>
2019-01-29 09:16:39 -0600

    podfile, react-native-navigation

--- packages/rn/index.js
+++ packages/rn/index.js
@@ -1,9 +1,20 @@
 /** @format */
 import '@babel/polyfill'
 
-// import { AppRegistry } from 'react-native'
-// import App from './App'
-// import { name as appName } from './app.json'
+import { Navigation } from "react-native-navigation"
+import App from './App'
 
-// AppRegistry.registerComponent(appName, () => App)
-export default from './storybook'
+
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "navigation.playground.WelcomeScreen"
+      }
+    }
+  });
+});
+
+// export default from './storybook'
