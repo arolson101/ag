0efd7727874144f1b02c09a58b510ee2e0a8e8f3
arolson101 <andrew@olsononline.org>
2019-03-22 16:26:38 -0500

    prettier

--- packages/db/typings/icojs.d.ts
+++ packages/db/typings/icojs.d.ts
@@ -1,14 +1,14 @@
 declare module 'icojs/index.js' {
   export interface ParsedImage {
-    width: number,
-    height: number,
-    bpp: number,
+    width: number
+    height: number
+    bpp: number
     buffer: ArrayBuffer
   }
 
-  export function isICO(source: ArrayBuffer | Buffer): Boolean;
+  export function isICO(source: ArrayBuffer | Buffer): Boolean
 
-  export function parse(buffer: ArrayBuffer | Buffer, mime?: string): Promise<Array<ParsedImage>>;
+  export function parse(buffer: ArrayBuffer | Buffer, mime?: string): Promise<ParsedImage[]>
 
-  export function parseSync(buffer: ArrayBuffer | Buffer, mime?: string): Array<ParsedImage>;
+  export function parseSync(buffer: ArrayBuffer | Buffer, mime?: string): ParsedImage[]
 }
