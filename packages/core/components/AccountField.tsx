0661e25b8fac3101267b1858b563d3ff8d349f38
Andrew Olson <andrew@olsononline.org>
2019-07-06 16:30:44 -0500

    add bank name to accountfield

--- packages/core/components/AccountField.tsx
+++ packages/core/components/AccountField.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { SelectFieldItem, typedFields, useSelector, useUi } from '../context'
+import { defineMessages } from 'react-intl'
+import { SelectFieldItem, typedFields, useIntl, useSelector, useUi } from '../context'
 import { selectors } from '../reducers'
 
 interface Props {
@@ -9,9 +10,17 @@ interface Props {
 
 export const AccountField = Object.assign(
   React.memo<Props>(function _AccountField({ field, label }) {
+    const intl = useIntl()
     const { SelectField } = useUi()
     const accounts = useSelector(selectors.getAccounts)
-    const items: SelectFieldItem[] = accounts.map(acct => ({ label: acct.name, value: acct.id }))
+    const getBank = useSelector(selectors.getBank)
+    const items: SelectFieldItem[] = accounts.map(acct => {
+      const intlContext = { bank: getBank(acct.bankId)!.name, account: acct.name }
+      return {
+        label: intl.formatMessage(messages.format, intlContext),
+        value: acct.id,
+      }
+    })
 
     return <SelectField field={field} label={label} items={items} />
   }),
@@ -19,3 +28,10 @@ export const AccountField = Object.assign(
     displayName: 'AccountField',
   }
 )
+
+const messages = defineMessages({
+  format: {
+    id: 'AccountField.format',
+    defaultMessage: '{bank} - {account}',
+  },
+})
