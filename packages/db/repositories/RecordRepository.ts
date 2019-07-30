f575120dfb9c3b5781f9b28abf8767f638b16dcd
arolson101 <andrew@olsononline.org>
2019-07-30 16:58:58 -0500

    don't modify input array

--- packages/db/repositories/RecordRepository.ts
+++ packages/db/repositories/RecordRepository.ts
@@ -16,22 +16,18 @@ export abstract class RecordRepository<T extends DbEntity<any>> extends Abstract
   }
 
   async getByIds(ids: string[]): Promise<Record<string, T>> {
-    const maxQuery = 10
     const ret: Record<string, T> = {}
     // log('getByIds')
-    while (ids.length > 0) {
-      const slice = ids.splice(0, maxQuery)
-      const res = await this.createQueryBuilder('e')
-        .whereInIds(slice)
-        .getMany()
-      if (!res) {
-        throw new Error('no results')
-      }
-
-      res.forEach(elt => {
-        ret[elt.id] = elt
-      })
+    const res = await this.createQueryBuilder('e')
+      .whereInIds(ids)
+      .getMany()
+    if (!res) {
+      throw new Error('no results')
     }
+
+    res.forEach(elt => {
+      ret[elt.id] = elt
+    })
     return ret
   }
 
